59203ace0ae9e0ddf4d3e4e046f94fd425379380
Rinaldi <rinaldiamfine@gmail.com>
2022-09-23 10:41:27 +0700

    Update  request for order

--- app/order/models.py
+++ app/order/models.py
@@ -1,5 +1,58 @@
-from typing import Dict, List, Optional
-from pydantic import BaseModel, Field
+from app.tools.database import DatabaseManager
 
-class DashboardModel(BaseModel):
-    '''Dashboard Model'''
+class OrderModel:
+    def __init__(self) -> None:
+        self.db  = DatabaseManager()
+
+    def get_order(self):
+        try:
+            cursor = self.db.connection.cursor()
+            cursor.execute("SELECT * FROM orders where is_active = 1")
+            columns = [column[0] for column in cursor.description]
+            result = [
+                dict(zip(columns, row))
+                for row in cursor.fetchall()
+            ]
+            return True, result
+        except Exception as e:
+            return False, str(e)
+
+    def get_order_by_id(self, id):
+        try:
+            cursor = self.db.connection.cursor()
+            cursor.execute("SELECT * FROM orders WHERE id = %s", (id,))
+            columns = [column[0] for column in cursor.description]
+            result = [
+                dict(zip(columns, row))
+                for row in cursor.fetchall()
+            ]
+            return True, result
+        except Exception as e:
+            return False, str(e)
+
+    def create_order(self, data):
+        try:
+            cursor = self.db.connection.cursor()
+            cursor.execute("INSERT INTO orders (product_id, employee_id, qty, start_date, end_date, status) VALUES (%s, %s, %s, %s, %s, %s)", (data['product_id'], data['employee_id'], data['qty'], data['start_date'], data['end_date'], data['status']))
+            self.db.connection.commit()
+            return True, "Order created successfully"
+        except Exception as e:
+            return False, str(e)
+
+    def update_order(self, id, data):
+        try:
+            cursor = self.db.connection.cursor()
+            cursor.execute("UPDATE orders SET product_id = %s, employee_id = %s, qty = %s, start_date = %s, end_date = %s, status = %s WHERE id = %s", (data['product_id'], data['employee_id'], data['qty'], data['start_date'], data['end_date'], data['status'], id))
+            self.db.connection.commit()
+            return True, "Order updated successfully"
+        except Exception as e:
+            return False, str(e)
+
+    def delete_order(self, id):
+        try:
+            cursor = self.db.connection.cursor()
+            cursor.execute("UPDATE orders SET is_active=0 WHERE id = %s", (id,))
+            self.db.connection.commit()
+            return True, "Order deleted successfully"
+        except Exception as e:
+            return False, str(e)
--- app/order/routes.py
+++ app/order/routes.py
@@ -6,66 +6,116 @@ import json
 import http.client
 from fastapi import Response
 from app.tools.background import app_background
+from app.order.models import OrderModel
 
 OrderRoute = APIRouter()
-
 websites = Jinja2Templates(directory="app/templates")
 
 @OrderRoute.get("/order", tags=['Orders'])
 def order_list(request: Request):
     '''Order List'''
-    res = {
-        "data": [
-            {"id": 1, "name": "John Doe", "age": 42},
-            {"id": 2, "name": "Jane Doe", "age": 36}
-        ]
-    }
-    return Response(
-        content=json.dumps(str(res)),
-        status_code=http.client.OK,
-        media_type='application/json'
-    )
-    
-@OrderRoute.post("/order", tags=['Inventories'])
-def order_create(request: Request):
-    '''Create Order'''
-    res = "Order Created"
-    return Response(
-        content=json.dumps(str(res)),
-        status_code=http.client.OK,
-        media_type='application/json'
-    )
-    
-@OrderRoute.get("/order/{id}", tags=['Inventories'])
+    try:
+        order = OrderModel()
+        status, res = order.get_order()
+        if not status:
+            return Response(content=res, status_code=400)
+        return Response(
+            content=json.dumps(res),
+            status_code=http.client.OK,
+            media_type='application/json'
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            content=json.dumps({'message': 'Error'}),
+            status_code=http.client.INTERNAL_SERVER_ERROR,
+            media_type='application/json'
+        )
+
+
+@OrderRoute.get("/order/{id}", tags=['Orders'])
 def order_detail(request: Request, id: int):
     '''Order Detail'''
-    res = {
-        "data": [
-            {"id": id, "name": "John Doe", "age": 42},
-        ]
-    }
-    return Response(
-        content=json.dumps(str(res)),
-        status_code=http.client.OK,
-        media_type='application/json'
-    )
+    try:
+        order = OrderModel()
+        status, res = order.get_order_by_id(id)
+        if not status:
+            return Response(content=res, status_code=400)
+        return Response(
+            content=json.dumps(res),
+            status_code=http.client.OK,
+            media_type='application/json'
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            content=json.dumps({'message': 'Error'}),
+            status_code=http.client.INTERNAL_SERVER_ERROR,
+            media_type='application/json'
+        )
     
-@OrderRoute.put("/order/{id}", tags=['Inventories'])
-def order_update(request: Request, id: int):
+@OrderRoute.post("/order", tags=['Orders'])
+async def order_create(request: Request):
+    '''Create Order'''
+    try:
+        data = await request.json()
+        order = OrderModel()
+        status, res = order.create_order(data)
+        if not status:
+            return Response(content=res, status_code=400)
+        return Response(
+            content=json.dumps(res),
+            status_code=http.client.OK,
+            media_type='application/json'
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            content=json.dumps({'message': 'Error'}),
+            status_code=http.client.INTERNAL_SERVER_ERROR,
+            media_type='application/json'
+        )
+
+    
+@OrderRoute.put("/order/{id}", tags=['Orders'])
+async def order_update(request: Request, id: int):
     '''Update Order'''
-    res = "Order Updated"
-    return Response(
-        content=json.dumps(str(res)),
-        status_code=http.client.OK,
-        media_type='application/json'
-    )
+    try:
+        data = await request.json()
+        order = OrderModel()
+        status, res = order.update_order(id, data)
+        if not status:
+            return Response(content=res, status_code=400)
+        return Response(
+            content=json.dumps(res),
+            status_code=http.client.OK,
+            media_type='application/json'
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            content=json.dumps({'message': 'Error'}),
+            status_code=http.client.INTERNAL_SERVER_ERROR,
+            media_type='application/json'
+        )
     
-@OrderRoute.delete("/order/{id}", tags=['Inventories'])
+@OrderRoute.delete("/order/{id}", tags=['Orders'])
 def order_delete(request: Request, id: int):
     '''Delete Order'''
-    res = "Order Deleted"
-    return Response(
-        content=json.dumps(str(res)),
-        status_code=http.client.OK,
-        media_type='application/json'
-    )
+    try:
+        order = OrderModel()
+        status, res = order.delete_order(id)
+        if not status:
+            return Response(content=res, status_code=400)
+        return Response(
+            content=json.dumps(res),
+            status_code=http.client.OK,
+            media_type='application/json'
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            content=json.dumps({'message': 'Error'}),
+            status_code=http.client.INTERNAL_SERVER_ERROR,
+            media_type='application/json'
+        )
