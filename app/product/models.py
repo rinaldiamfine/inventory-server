81e27b3268972e5e98e11dee9d7ee10c48b9e2e8
Rinaldi <rinaldiamfine@gmail.com>
2022-09-23 10:16:59 +0700

    Update request products

---
+++ app/product/models.py
@@ -0,0 +1,58 @@
+from app.tools.database import DatabaseManager
+
+class ProductModel:
+    def __init__(self):
+        pass
+
+    def get_product(self):
+        try:
+            cursor = self.db.connection.cursor()
+            cursor.execute("SELECT * FROM products where is_active = 1")
+            columns = [column[0] for column in cursor.description]
+            result = [
+                dict(zip(columns, row))
+                for row in cursor.fetchall()
+            ]
+            return True, result
+        except Exception as e:
+            return False, str(e)
+
+    def get_product_by_id(self, id):
+        try:
+            cursor = self.db.connection.cursor()
+            cursor.execute("SELECT * FROM products WHERE id = %s", (id,))
+            columns = [column[0] for column in cursor.description]
+            result = [
+                dict(zip(columns, row))
+                for row in cursor.fetchall()
+            ]
+            return True, result
+        except Exception as e:
+            return False, str(e)
+
+    def create_product(self, data):
+        try:
+            cursor = self.db.connection.cursor()
+            cursor.execute("INSERT INTO products (name, code, qty) VALUES (%s, %s, %s)", (data['name'], data['code'], data['qty']))
+            self.db.connection.commit()
+            return True, "Product created successfully"
+        except Exception as e:
+            return False, str(e)
+
+    def update_product(self, id, data):
+        try:
+            cursor = self.db.connection.cursor()
+            cursor.execute("UPDATE products SET name = %s, code = %s, qty = %s WHERE id = %s", (data['name'], data['code'], data['qty'], id))
+            self.db.connection.commit()
+            return True, "Product updated successfully"
+        except Exception as e:
+            return False, str(e)
+
+    def delete_product(self, id):
+        try:
+            cursor = self.db.connection.cursor()
+            cursor.execute("UPDATE products SET is_active=0 WHERE id = %s", (id,))
+            self.db.connection.commit()
+            return True, "Product deleted successfully"
+        except Exception as e:
+            return False, str(e)
--- app/product/routes.py
+++ app/product/routes.py
@@ -5,66 +5,119 @@ import json
 import http.client
 from fastapi import Response
 from app.tools.background import app_background
+from app.product.models import ProductModel
 
 ProductRoute = APIRouter()
 
 @ProductRoute.get("/product", tags=['Products'])
 def product_list(request: Request):
     '''Product List'''
-    res = {
-        "data": [
-            {"id": 1, "name": "John Doe", "age": 42},
-            {"id": 2, "name": "Jane Doe", "age": 36}
-        ]
-    }
-    return Response(
-        content=json.dumps(str(res)),
-        status_code=http.client.OK,
-        media_type='application/json'
-    )
+    try:
+        product = ProductModel()
+        status, res = product.get_product()
+        if not status:
+            return Response(content=res, status_code=400)
+
+        print("Products: ", res)
+        return Response(
+            content=json.dumps(res),
+            status_code=http.client.OK,
+            media_type='application/json'
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            content=json.dumps({'message': 'Error'}),
+            status_code=http.client.INTERNAL_SERVER_ERROR,
+            media_type='application/json'
+        )
     
 @ProductRoute.get("/product/{id}", tags=['Products'])
 def product_detail(request: Request, id: int):
     '''Product Detail'''
-    res = {
-        "data": {
-            "id": id,
-            "name": "John Doe",
-            "age": 42
-        }
-    }
-    return Response(
-        content=json.dumps(str(res)),
-        status_code=http.client.OK,
-        media_type='application/json'
-    )
+    try:
+        product = ProductModel()
+        status, res = product.get_product_by_id(id)
+        if not status:
+            return Response(content=res, status_code=400)
+
+        return Response(
+            content=json.dumps(res),
+            status_code=http.client.OK,
+            media_type='application/json'
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            content=json.dumps({'message': 'Error'}),
+            status_code=http.client.INTERNAL_SERVER_ERROR,
+            media_type='application/json'
+        )
     
 @ProductRoute.post("/product", tags=['Products'])
-def product_create(request: Request):
+async def product_create(request: Request):
     '''Product Create'''
-    res = "Product Create"
-    return Response(
-        content=json.dumps(str(res)),
-        status_code=http.client.OK,
-        media_type='application/json'
-    )
+    try:
+        product = ProductModel()
+        datas = await request.json()
+        status, res = product.create_product(datas)
+        if not status:
+            return Response(content=res, status_code=400)
+
+        return Response(
+            content=json.dumps(res),
+            status_code=http.client.CREATED,
+            media_type='application/json'
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            content=json.dumps({'message': 'Error'}),
+            status_code=http.client.INTERNAL_SERVER_ERROR,
+            media_type='application/json'
+        )
     
 @ProductRoute.put("/product/{id}", tags=['Products'])
-def product_update(request: Request, id: int):
+async def product_update(request: Request, id: int):
     '''Product Update'''
-    res = "Product Update"
-    return Response(
-        content=json.dumps(str(res)),
-        status_code=http.client.OK,
-        media_type='application/json'
-    )
+    try:
+        product = ProductModel()
+        datas = await request.json()
+        status, res = product.update_product(id, datas)
+        if not status:
+            return Response(content=res, status_code=400)
+
+        return Response(
+            content=json.dumps(res),
+            status_code=http.client.OK,
+            media_type='application/json'
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            content=json.dumps({'message': 'Error'}),
+            status_code=http.client.INTERNAL_SERVER_ERROR,
+            media_type='application/json'
+        )
     
 @ProductRoute.delete("/product/{id}", tags=['Products'])
 def product_delete(request: Request, id: int):
     '''Product Delete'''
-    res = "Product Delete"
-    return Response(
-        content=json.dumps(str(res)),
-        status_code=http.client.OK,
-        media_type='application/json'
-    )
+    try:
+        product = ProductModel()
+        status, res = product.delete_product(id)
+        if not status:
+            return Response(content=res, status_code=400)
+
+        return Response(
+            content=json.dumps(res),
+            status_code=http.client.OK,
+            media_type='application/json'
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            content=json.dumps({'message': 'Error'}),
+            status_code=http.client.INTERNAL_SERVER_ERROR,
+            media_type='application/json'
+        )
